38f01b5e26bf4a5f3ee2facf6175e6df727cb23a
LDagtekin <302154576@student.rocmondriaan.nl>
2023-09-08 09:11:58 +0200

    javascript opdrachten

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,109 @@
+console.log("main.js werkt");
+
+// opdr. 1a
+let a = 5;
+let b = 3;
+
+let result = a + b;
+console.log("opdr.1a is:", result);
+
+// opdr. 1b
+a = 12;
+b = 8;
+
+result = a + b;
+console.log("opdr.1b is:", result);
+
+// opdr. 1c
+let c = 4;
+let x = 5;
+result = c + x;
+console.log("opdr.1c is:", result);
+
+// opdr. 2a
+x = 9;
+let y = 6;
+
+result = x*y;
+console.log("opdr.2a:", result);
+
+// opdr. 2b
+x = 13;
+y = 2356;
+
+result = x*y;
+console.log("opdr.2b:", result);
+
+// opdr. 2c
+x = 5;
+y = 98;
+
+result = x*y;
+console.log("opdr.2c:", result);
+
+/////////////////////////////////
+// Opdr. 3
+let salary = 2000;
+let raise = 0.05;
+let salaryRaise = salary * raise; 
+
+console.log("nieuw salaris is", salaryRaise);
+
+// Opdr. 4
+x = 4;
+y = 2;
+
+result = x/y;
+console.log("opdr.4:",result);
+
+// Opdr. 5
+x = 4;
+y = 0;
+
+result = x/y;
+console.log("Opdr.5:", result);
+
+// Opdr. 6
+let house = 400000;
+let interest = 0.025;
+let amount = house*interest
+
+console.log("te betalen rente:", amount);
+
+let month = amount/12;
+console.log("per maand aan rente:", month);
+
+// Opdr. 7
+let jacket = 100;
+let tax = 1.21;
+
+let price = jacket*tax;
+console.log("prijs jas:", price);
+
+// Opdr. 8 + 9
+let firstname = "Jonah";
+let surname = "Mengsteab";
+
+console.log("Naam is:", firstname , surname);
+
+// Opdr. 10
+let on = true
+let off = false
+let waar = true
+let onwaar = false
+let win = true
+let lose = false
+
+console.log("aan is", true);
+console.log("uit is", false);
+console.log("waar is", true);
+console.log("onwaar is", false);
+console.log("winnen is", true);
+console.log("verliezen is", false);
+
+// Opdr. 11
+console.log("aan is", 2/2);
+console.log("aan is", 2%2);
+console.log("aan is", 3%2);
+console.log("aan is", 4%2);
+console.log("aan is", 5%2);
---
+++ Javascript Comparisons/main.js
@@ -0,0 +1,140 @@
+// 1
+let x = 5
+let y = 5
+console.log("5 = 5", x == y);
+
+// 2
+x = "apple"
+y = "sinnaasappel"
+console.log("apple != sinaasappel", x != y);
+
+// 3
+x = 10
+y = 5
+console.log("10 > 5", x > y);
+
+// 4
+x = 3
+y = 7
+console.log("3 < 7", x < y);
+
+// 5
+x = 8
+y = 8
+console.log("8 >= 8", x >= y);
+
+// 6
+x = 2
+y = 4
+console.log("2 <= 4", x <= y);
+
+// 7
+x = 7
+y = 7
+console.log("7 === 7", x === y);
+
+// 8
+x = "hallo"
+y = "wereld"
+console.log("hallo !== wereld", x !== y);
+
+// 9
+x = "true"
+y = 1
+console.log("true == 1", x == y);
+
+// 10
+x = "false"
+y = 0
+console.log("false != 0", x != y);
+
+// 11
+x = 6
+y = "3"
+console.log("6 > 3", x > y);
+
+// 12
+x = 2
+y = 5
+console.log("2 <= 5", x <= y);
+
+// 13
+x = 10
+y = "10"
+console.log("10 === 10", x === y);
+
+// 14
+x = "appel"
+y = 5
+console.log("appel != 5", x !== y);
+
+// 15
+x = "null"
+y = "undefined"
+console.log("null == undefined", x == y);
+
+// 16
+x = "NaN"
+y = "NaN"
+console.log("NaN != NaN", x != y);
+
+// 17
+x = 4
+y = 7
+console.log("4 >= 7", x >= y);
+
+// 18
+x = 9
+y = 3
+console.log("9 <= 3", x <= y);
+
+// 19
+x = "hond"
+y = "hond"
+console.log("hond === hond", x === y);
+
+// 20
+x = "true"
+y = "false"
+console.log("true !== false", x !== y);
+
+// 21
+x = 10
+y = 5
+console.log("10 != 5", x != y);
+
+// 22
+x = "auto"
+y = "auto"
+console.log("auto == auto", x == y);
+
+// 23
+x = 3
+y = 7
+let z = 10
+console.log("7 > 3 && 7 < 10", y > x && y < z);
+
+// 24
+x = 5
+y = 3
+console.log("5 < 3 || 5 == 5", x < y || x == x);
+
+// 27
+x = 8
+y = "8"
+console.log("8 !== 8", x !== x);
+
+// 29
+x = 8
+y = 10
+console.log("x > 10", x > y);
+
+// 30
+x = 1
+y = 3
+console.log("y > 1 && y < 100", y > 1 && y < 100);
+
+// 31
+x = "z"
+y = 3
+console.log("z=== ", z=== "" );
