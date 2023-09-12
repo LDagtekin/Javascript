d50b2865c2ff8d51d6820062c1904bb74309c0eb
LDagtekin <143403177+LDagtekin@users.noreply.github.com>
2023-09-12 12:48:56 +0200

    Javascript week 3

---
+++ JS Week 3/main.js
@@ -0,0 +1,14 @@
+console.log("main.js is loaded");
+
+
+
+
+
+let btnChange = document.querySelector('.change-btn');
+btnChange.addEventListener('click', function(){
+    let fullName = document.querySelector('.fullname')
+    fullName.textContent = 'Latif Dagtekin';
+});
+
+let slfChange = document.querySelector('.changeSelf');
+slfChange.addEventListener
