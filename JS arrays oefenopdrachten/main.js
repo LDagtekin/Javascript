916472dd56b89c6e761674df4cc393815a9288dc
LDagtekin <302154576@student.rocmondriaan.nl>
2023-09-28 15:10:09 +0200

    JS while opdrachten

--- JS arrays oefenopdrachten/main.js
+++ JS arrays oefenopdrachten/main.js
@@ -1,5 +1,6 @@
 console.log("main.js is loaded");
 
+// opdr.1
 let list1 = [];
 console.log("list1 is:", list1);
 list1.push(4);
@@ -10,6 +11,7 @@ list1.push(-1);
 list1.push(99);
 console.log("list1 met push", list1);
 
+// opdr. 2
 let list2 = [];
 console.log("list2 is:", list2);
 list2.push(0.33);
@@ -21,6 +23,7 @@ list2.push(-2.56);
 list2.push(-17.4);
 console.log("list2 met push", list2);
 
+// opdr. 3
 let colour = [];
 console.log("Kleuren:", colour);
 colour.push('rood');
@@ -30,26 +33,34 @@ colour.push('blauw');
 colour.push('roze');
 console.log("Kleuren met push", colour);
 
+// opdr.4
 list1[3] = 21;
 console.log("list 1 is", list1);
 
+// opdr. 5
 list2[0] = 3.3;
 list2[6] = -1.74;
 console.log("list 2 is", list2);
 
+//opdracht 6
+list3[3] = "paars";
+console.log("list 3 is", list3);
+
+// opdr. 7
 colour.push('zwart')
 console.log("Update kleuren met push", colour)
 
+// opdr. 8
 list1[0] = 0;
 console.log("Update list1:", list1);
 
+// opdr. 9
 list2.pop(0.33);
 list2.pop(-17.4);
 console.log("List2 met pop", list2)
 
+// opdr. 10
 var items = document.querySelector('items');
-
- 
 
 function addItemToList() {
   var newItem = prompt("Noem een film");
---
+++ JS while opdrachten/main.js
@@ -0,0 +1,96 @@
+console.log("main.js werkt");
+
+// Opdr. 1
+let counter = 0;
+while(counter < 10) {
+    console.log("counter is:",counter)
+
+    counter = counter + 1;
+}
+console.log("Einde loop");
+
+// Opdr. 2
+ counter = 2;
+while(counter < 21) {
+    console.log("counter is:",counter)
+
+    counter = counter + 1;
+}
+console.log("Einde loop");
+
+// Opdr. 3
+ counter = 10;
+while(counter > -1) {
+    console.log("counter is:",counter)
+
+    counter = counter - 1;
+}
+console.log("Einde loop");
+
+// Opdr. 4
+counter = 40;
+while(counter > 3) {
+    console.log("counter is:",counter)
+
+    counter = counter - 1;
+}
+console.log("Einde loop");
+
+// Opdr. 5
+counter = 8;
+while(counter > 0) {
+    console.log("Ik ben een student van ROC Mondriaan:",counter)
+
+    counter = counter - 1;
+}
+console.log("Einde loop");
+
+// Opdr. 6
+counter = 0;
+while(counter < 13) {
+    console.log("Ik ben loop:",counter)
+
+    counter = counter + 1;
+}
+console.log("Einde loop");
+
+// Opdr. 7
+counter = 0;
+while(counter > -3) {
+    console.log("Counter is",counter)
+
+    counter = counter + 1;
+
+    if (counter >= 10) {
+        break
+    }
+    
+}
+console.log("Einde loop");
+
+// Opdr. 8
+counter = 0;
+while(counter < 21) {
+    console.log("Loop",counter)
+
+    counter = counter + 1;
+    if(counter >= 11) {
+        break
+    }
+}
+console.log("Einde loop");
+
+// Opdr. 9
+counter = 0;
+while(counter < 21) {
+    console.log("Dit is zin 1. Dit is zin 2.",counter)
+
+    counter = counter + 1;
+    if(counter >= 11) {
+        console.log("Dit is zin 2.", counter)
+        break
+       
+    }
+
+}
+
