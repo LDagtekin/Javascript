e91251c7cf2b27450a6b567ba0e69fc322625beb
LDagtekin <302154576@student.rocmondriaan.nl>
2023-11-09 12:54:25 +0100

    Oefenopdrachten

---
+++ JS oefenopdrachten/main.js
@@ -0,0 +1,31 @@
+// Opdr. 1
+let salaris = 2000;
+let verhoging = 1.05;
+let antwoord = salaris * verhoging;
+
+console.log("Opdracht 1 is", antwoord);
+
+// Opdr. 2
+let prijs = 400000;
+let percentage = 1.025;
+let Waarde = prijs * percentage;
+
+console.log("Opdracht 2 is", Waarde);
+
+// Opdr. 3
+let prijsJas = 100;
+let btw = 1.21;
+let volPrijs = prijsJas * btw;
+
+console.log("Opdracht 3 is", volPrijs)
+
+// Opdr. 4
+let person = prompt("Wat ben jij?")
+if (person = "docent"){
+  alert("Jij bent een docent")
+}
+else{
+  alert("Jij bent gek")
+}
+
+// Opdr. 5
